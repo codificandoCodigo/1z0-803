ea9e73ffb4ebdd7e46cd25859e58b62b8901adb0
Ivan <ivanvillarroelp@gmail.com>
2017-06-24 17:38:34 -0400

    A

---
+++ 1 Conceptos basicos/import e import estatico de clases estaticas/src/Package1/SecondClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Package1;
+
+/**
+ *
+ * @author zabo
+ */
+public class SecondClass {
+    //clase estatica
+    public static class Stat {
+        //variable estatica
+        public static int numero = 10;
+        //metodo estatico
+        public static void imprimir() {
+            System.out.println("imprimiendo");
+        }
+    }
+}
+   
+    
