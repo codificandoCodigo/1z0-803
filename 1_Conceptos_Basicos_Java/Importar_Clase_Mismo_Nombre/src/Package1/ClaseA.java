6e2692ec213fc5e135796f08fcf659f29383c795
zabo <zabo@linux-kvfr.suse>
2017-11-05 17:59:46 -0300

    import clase

---
+++ 1_Conceptos_Basicos_Java/Importar_Clase_Mismo_Nombre/src/Package1/ClaseA.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Package1;
+/**
+ *
+ * @author zabo
+ */
+public class ClaseA {
+    public static void main(String[] args) {
+       Package2.ClaseA ca = new Package2.ClaseA();
+        System.out.println("nombre "+ca.name);
+               
+    }
+}
